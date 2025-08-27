6d54aadac92d2659deef10cc4ad54753a77c66f5
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2025-08-27 11:23:57 -0400

    hamburger

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// Placeholder
+const menuCheckbox = document.getElementById('menuCheckbox');
+const mainContent = document.querySelector('main');
+
+menuCheckbox.addEventListener('change', () => {
+    mainContent.classList.toggle('menu-open', menuCheckbox.checked);
+});
